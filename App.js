c380d2f3ec0cab6220a92aa55144690b1006edb4
StijnVP <stijnvanpeborgh@gmail.com>
2023-11-06 00:32:07 +0100

    update: redesign

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, ScrollView } from 'react-native';
 //Task = todo item
 
 export default function App() {
@@ -9,54 +9,106 @@ export default function App() {
     console.log(enteredText);
   }
   const addTaskHandler = () => {
-    console.log("clicked");
+    console.log("should add task");
   }
 
   return (
-    <View style={styles.screen}>
+    <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="add task..."
-          style={styles.input}
-          clearButtonMode="always"
-          onChangeText={taskInputHandler}
-        />
-        <Button title="ADD" onPress={addTaskHandler} />
-      </View>
+      <Text style={styles.heading}>To Do App</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter task"
+        onChangeText={taskInputHandler}
+      />
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => addTaskHandler}>
+        <Text style={styles.addButtonText}>Add Task</Text>
+      </TouchableOpacity>
       <ScrollView>
-        <View style={styles.listItem}>
-          <Text>Task 1</Text>
-        </View>
-        <View style={styles.listItem}>
-          <Text>Task 2</Text>
-        </View>
+        <View style={styles.task}>
+          <Text style={styles.itemList}>taskname goes here</Text>
+          <View style={styles.taskButtons}>
+            <TouchableOpacity
+              onPress={() => { console.log("should delete task") }}>
+              <Text style={styles.deleteButton}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        </View >
+        <View style={styles.task}>
+          <Text style={styles.itemList}>taskname goes here</Text>
+          <View style={styles.taskButtons}>
+            <TouchableOpacity
+              onPress={() => { console.log("should delete task") }}>
+              <Text style={styles.deleteButton}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        </View >
       </ScrollView>
     </View >
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50,
+  container: {
+    flex: 1,
+    padding: 40,
+    marginTop: 40,
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 7,
+    color: "green",
   },
   input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
+    borderWidth: 3,
+    borderColor: "#ccc",
     padding: 10,
     marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 18,
   },
-  listItem: {
+  addButton: {
+    backgroundColor: "green",
     padding: 10,
-    marginVertical: 10,//bestaat niet in CSS
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  task: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+    fontSize: 18,
+  },
+  itemList: {
+    fontSize: 19,
+  },
+  taskButtons: {
+    flexDirection: "row",
+  },
+  editButton: {
+    marginRight: 10,
+    color: "green",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  deleteButton: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 18,
   }
 });
